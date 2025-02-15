b2b00df6b227130198e402ee9a818464135a3afd
RouQandel <rawanqandel62@gmail.com>
2025-02-15 14:44:00 +0200

    Add_Order_CustomerEntities

---
+++ AccessDataLayer/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AccessDataLayer.Entities;
+
+public class Order
+{
+    public int OrderId { get; set; } // Primary Key
+    public DateTime OrderData { get; set; } = DateTime.Now;
+    public int TotalAmount { get; set; }
+
+    // Navigation Property
+
+    public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+
+    // Foreign Key
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+}
