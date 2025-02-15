e2892a04b4534242bf67966baf6be87101669cd0
RouQandel <rawanqandel62@gmail.com>
2025-02-15 15:44:45 +0200

    Add_CategoryEntityAndConfiguration

---
+++ AccessDataLayer/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccessDataLayer.Entities;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; } // Primary Key
+    public string CategoryName { get; set; } = string.Empty;
+    // Navigation Property
+    public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+}
--- AccessDataLayer/Entities/Product.cs
+++ AccessDataLayer/Entities/Product.cs
@@ -1,5 +1,15 @@
-﻿namespace AccessDataLayer.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AccessDataLayer.Entities;
 
 public class Product
 {
+
+
+    //navigation property
+
+    //foreign key
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; } 
+    public Category Category { get; set; } = null!;
 }
