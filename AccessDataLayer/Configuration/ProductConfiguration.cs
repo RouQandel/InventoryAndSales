d12c1b1465f3bbca60800a63ad0a3d37985e4198
Mohamedmahdy404 <mohamedmahdy3162@gmail.com>
2025-02-17 22:11:42 +0200

    Modify to add Flout api to product and remove Required from the props

--- AccessDataLayer/Configuration/ProductConfiguration.cs
+++ AccessDataLayer/Configuration/ProductConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using AccessDataLayer.Entities;
+using AccessDataLayer.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,5 +11,19 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
         builder.HasKey(o => o.ProductId);
 
+        builder.Property(o => o.ProductName)
+            .IsRequired().HasMaxLength(50);
+
+        builder.Property(o => o.UnitPrice)
+            .IsRequired();
+
+        builder.Property(o => o.QuantityInStock)
+            .IsRequired();
+
+        builder.Property(o => o.Category)
+          .HasConversion(v => v.ToString(), //converts the enum to its string representation when storing it in the database.
+        v => (CategoryEnum)Enum.Parse(typeof(CategoryEnum), v)) //converts the string back to the enum when reading from the database.
+          .HasDefaultValue(CategoryEnum.Other.ToString()) // Set default value as "Other"
+          .IsRequired(false); // Optional property
     }
 }
