21302efcd533cbd5bfd963f94b98d7fedf3b9dfa
RouQandel <rawanqandel62@gmail.com>
2025-02-17 15:34:44 +0200

    updated

--- AccessDataLayer/DbContext/AppDbContext.cs
+++ AccessDataLayer/DbContext/AppDbContext.cs
@@ -1,19 +1,20 @@
-﻿
-using AccessDataLayer.Entities;
+﻿using AccessDataLayer.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace AccessDataLayer;
 
 public class AppDbContext : DbContext
 {
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Product> Products { get; set; }
-    public DbSet<Order>  Orders { get; set; }
-    public DbSet<Customer> Customers  { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Customer> Customers { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
     }
-
 
 }
