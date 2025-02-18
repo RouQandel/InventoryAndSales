45a4e940ac2afd104d387e73f8f87fd45c3d1155
wesamriad <wesoryad2@gmail.com>
2025-02-18 20:34:52 +0200

    updatePresantation

--- Presantation/Form1.Designer.cs
+++ Presantation/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1394, 450);
+            ClientSize = new Size(1394, 567);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
