159a7ddfcbf41fcbe08a6a0baa5cc149b99ba539
Khaled-Altabey <khaledaltabey55@gmail.com>
2025-02-22 20:24:04 +0200

    add-forms-updated

--- Presantation/Form1.Designer.cs
+++ Presantation/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -20,68 +20,225 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-<<<<<<< HEAD
-            button1 = new Button();
-            label1 = new Label();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(310, 249);
-            button1.Name = "button1";
-            button1.Size = new Size(141, 45);
-            button1.TabIndex = 0;
-            button1.Text = "Start";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(359, 121);
-            label1.Name = "label1";
-            label1.Size = new Size(35, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Hello";
-            label1.Click += label1_Click;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1370, 450);
-            Controls.Add(label1);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
-=======
-            SuspendLayout();
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1394, 567);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
->>>>>>> 45a4e940ac2afd104d387e73f8f87fd45c3d1155
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label2 = new Label();
+			label3 = new Label();
+			label4 = new Label();
+			label5 = new Label();
+			label6 = new Label();
+			label7 = new Label();
+			label8 = new Label();
+			label9 = new Label();
+			label10 = new Label();
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			pictureBox1 = new PictureBox();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
+			// 
+			// label2
+			// 
+			label2.Font = new Font("SansSerif", 22.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+			label2.ForeColor = Color.White;
+			label2.Location = new Point(69, 83);
+			label2.Name = "label2";
+			label2.Size = new Size(335, 46);
+			label2.TabIndex = 1;
+			label2.Text = " Key Features:";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("SansSerif", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+			label3.ForeColor = Color.White;
+			label3.Location = new Point(114, 151);
+			label3.Name = "label3";
+			label3.Size = new Size(335, 31);
+			label3.TabIndex = 2;
+			label3.Text = "1 - Product Management:";
+			label3.Click += label3_Click;
+			// 
+			// label4
+			// 
+			label4.Font = new Font("SansSerif", 16.1999989F);
+			label4.ForeColor = Color.White;
+			label4.Location = new Point(164, 205);
+			label4.Name = "label4";
+			label4.Size = new Size(1173, 45);
+			label4.TabIndex = 3;
+			label4.Text = "- Display All Products: View a comprehensive list of all available products with detailed information such as name, category, price, and stock quantity.";
+			label4.Click += label4_Click;
+			// 
+			// label5
+			// 
+			label5.Font = new Font("SansSerif", 16.1999989F);
+			label5.ForeColor = Color.White;
+			label5.Location = new Point(164, 251);
+			label5.Name = "label5";
+			label5.Size = new Size(1173, 45);
+			label5.TabIndex = 4;
+			label5.Text = "- Add New Products: Easily add new products to the inventory by entering relevant details like product name, description, price, and stock level.\r\n";
+			// 
+			// label6
+			// 
+			label6.Font = new Font("SansSerif", 16.1999989F);
+			label6.ForeColor = Color.White;
+			label6.Location = new Point(164, 295);
+			label6.Name = "label6";
+			label6.Size = new Size(1173, 45);
+			label6.TabIndex = 5;
+			label6.Text = "- Update Products: Edit product details to keep the inventory accurate and up-to-date.\r\n";
+			// 
+			// label7
+			// 
+			label7.Font = new Font("SansSerif", 16.1999989F);
+			label7.ForeColor = Color.White;
+			label7.Location = new Point(164, 340);
+			label7.Name = "label7";
+			label7.Size = new Size(1173, 45);
+			label7.TabIndex = 6;
+			label7.Text = "- Delete Products: Remove products that are no longer available or needed.\r\n";
+			// 
+			// label8
+			// 
+			label8.AutoSize = true;
+			label8.Font = new Font("SansSerif", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+			label8.ForeColor = Color.White;
+			label8.Location = new Point(114, 395);
+			label8.Name = "label8";
+			label8.Size = new Size(308, 31);
+			label8.TabIndex = 7;
+			label8.Text = "2 - Order Management:";
+			// 
+			// label9
+			// 
+			label9.Font = new Font("SansSerif", 16.1999989F);
+			label9.ForeColor = Color.White;
+			label9.Location = new Point(164, 443);
+			label9.Name = "label9";
+			label9.Size = new Size(1173, 45);
+			label9.TabIndex = 8;
+			label9.Text = "- Create Orders: Allow customers or staff to create new orders by selecting products, specifying quantities, and providing customer information.\r\n\r\n";
+			// 
+			// label10
+			// 
+			label10.Font = new Font("SansSerif", 16.1999989F);
+			label10.ForeColor = Color.White;
+			label10.Location = new Point(164, 487);
+			label10.Name = "label10";
+			label10.Size = new Size(1173, 45);
+			label10.TabIndex = 9;
+			label10.Text = "- Delete Orders: Cancel or remove orders that are no longer required.\r\n";
+			// 
+			// button1
+			// 
+			button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			button1.BackColor = Color.DodgerBlue;
+			button1.FlatStyle = FlatStyle.Flat;
+			button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+			button1.ForeColor = Color.White;
+			button1.Location = new Point(828, 889);
+			button1.Name = "button1";
+			button1.Size = new Size(135, 56);
+			button1.TabIndex = 10;
+			button1.Text = "Products";
+			button1.UseVisualStyleBackColor = false;
+			button1.Click += button1_Click_2;
+			// 
+			// button2
+			// 
+			button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			button2.BackColor = Color.DodgerBlue;
+			button2.FlatStyle = FlatStyle.Flat;
+			button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+			button2.ForeColor = Color.White;
+			button2.Location = new Point(1021, 889);
+			button2.Name = "button2";
+			button2.Size = new Size(136, 56);
+			button2.TabIndex = 11;
+			button2.Text = "Orders";
+			button2.UseVisualStyleBackColor = false;
+			button2.Click += button2_Click_1;
+			// 
+			// button3
+			// 
+			button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			button3.BackColor = Color.DodgerBlue;
+			button3.FlatStyle = FlatStyle.Flat;
+			button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+			button3.ForeColor = Color.White;
+			button3.Location = new Point(1220, 889);
+			button3.Name = "button3";
+			button3.Size = new Size(136, 56);
+			button3.TabIndex = 12;
+			button3.Text = "Back";
+			button3.UseVisualStyleBackColor = false;
+			button3.Click += button3_Click_1;
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			pictureBox1.BackColor = Color.White;
+			pictureBox1.BackgroundImageLayout = ImageLayout.None;
+			pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+			pictureBox1.Image = Properties.Resources.computer_inventory;
+			pictureBox1.Location = new Point(164, 602);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(412, 319);
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox1.TabIndex = 13;
+			pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(174, 217, 255);
+			ClientSize = new Size(1521, 978);
+			Controls.Add(pictureBox1);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(label10);
+			Controls.Add(label9);
+			Controls.Add(label8);
+			Controls.Add(label7);
+			Controls.Add(label6);
+			Controls.Add(label5);
+			Controls.Add(label4);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Margin = new Padding(3, 4, 3, 4);
+			Name = "Form1";
+			Text = "Form1";
+			WindowState = FormWindowState.Maximized;
+			Load += Form2_Load;
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-
-        private Button button1;
-        private Label label1;
-    }  
+		#endregion
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private Label label5;
+		private Label label6;
+		private Label label7;
+		private Label label8;
+		private Label label9;
+		private Label label10;
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private PictureBox pictureBox1;
+	}
 }
--- Presantation/Form2.Designer.cs
+++ Presantation/Form2.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form2
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -20,53 +20,435 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            button1 = new Button();
-            button2 = new Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(131, 217);
-            button1.Name = "button1";
-            button1.Size = new Size(146, 67);
-            button1.TabIndex = 0;
-            button1.Text = "Product";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(298, 323);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Back";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // Form2
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Name = "Form2";
-            Text = "Form2";
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			dataGridView1 = new DataGridView();
+			groupBox1 = new GroupBox();
+			comboBox1 = new ComboBox();
+			textBox4 = new TextBox();
+			textBox3 = new TextBox();
+			textBox2 = new TextBox();
+			label5 = new Label();
+			textBox1 = new TextBox();
+			label4 = new Label();
+			label3 = new Label();
+			label2 = new Label();
+			label1 = new Label();
+			button1 = new Button();
+			groupBox2 = new GroupBox();
+			comboBox2 = new ComboBox();
+			textBox8 = new TextBox();
+			textBox7 = new TextBox();
+			textBox6 = new TextBox();
+			textBox5 = new TextBox();
+			label10 = new Label();
+			label9 = new Label();
+			label8 = new Label();
+			label7 = new Label();
+			label6 = new Label();
+			button2 = new Button();
+			groupBox3 = new GroupBox();
+			textBox9 = new TextBox();
+			label11 = new Label();
+			button3 = new Button();
+			button4 = new Button();
+			button5 = new Button();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			groupBox1.SuspendLayout();
+			groupBox2.SuspendLayout();
+			groupBox3.SuspendLayout();
+			SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.Anchor = AnchorStyles.Bottom;
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(24, 498);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.RowHeadersWidth = 51;
+			dataGridView1.Size = new Size(1370, 364);
+			dataGridView1.TabIndex = 0;
+			dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Controls.Add(comboBox1);
+			groupBox1.Controls.Add(textBox4);
+			groupBox1.Controls.Add(textBox3);
+			groupBox1.Controls.Add(textBox2);
+			groupBox1.Controls.Add(label5);
+			groupBox1.Controls.Add(textBox1);
+			groupBox1.Controls.Add(label4);
+			groupBox1.Controls.Add(label3);
+			groupBox1.Controls.Add(label2);
+			groupBox1.Controls.Add(label1);
+			groupBox1.Controls.Add(button1);
+			groupBox1.Font = new Font("Segoe UI", 10F);
+			groupBox1.Location = new Point(54, 27);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new Size(365, 421);
+			groupBox1.TabIndex = 1;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Add Product";
+			// 
+			// comboBox1
+			// 
+			comboBox1.FormattingEnabled = true;
+			comboBox1.Location = new Point(171, 286);
+			comboBox1.Name = "comboBox1";
+			comboBox1.Size = new Size(158, 31);
+			comboBox1.TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			textBox4.Location = new Point(171, 230);
+			textBox4.Name = "textBox4";
+			textBox4.Size = new Size(158, 30);
+			textBox4.TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			textBox3.Location = new Point(171, 176);
+			textBox3.Name = "textBox3";
+			textBox3.Size = new Size(158, 30);
+			textBox3.TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(171, 123);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(158, 30);
+			textBox2.TabIndex = 4;
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Location = new Point(25, 230);
+			label5.Name = "label5";
+			label5.Size = new Size(92, 23);
+			label5.TabIndex = 3;
+			label5.Text = "Supplier Id";
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(171, 72);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(158, 30);
+			textBox1.TabIndex = 3;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(25, 289);
+			label4.Name = "label4";
+			label4.Size = new Size(79, 23);
+			label4.TabIndex = 3;
+			label4.Text = "Category";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(25, 176);
+			label3.Name = "label3";
+			label3.Size = new Size(140, 23);
+			label3.TabIndex = 3;
+			label3.Text = "Quantity in Stock";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(25, 126);
+			label2.Name = "label2";
+			label2.Size = new Size(47, 23);
+			label2.TabIndex = 3;
+			label2.Text = "Price";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(25, 75);
+			label1.Name = "label1";
+			label1.Size = new Size(121, 23);
+			label1.TabIndex = 1;
+			label1.Text = "Product Name";
+			label1.Click += label1_Click_1;
+			// 
+			// button1
+			// 
+			button1.AutoSize = true;
+			button1.BackColor = Color.DodgerBlue;
+			button1.FlatStyle = FlatStyle.Flat;
+			button1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold);
+			button1.ForeColor = Color.White;
+			button1.Location = new Point(198, 353);
+			button1.Name = "button1";
+			button1.Size = new Size(131, 51);
+			button1.TabIndex = 3;
+			button1.Text = "Add";
+			button1.UseVisualStyleBackColor = false;
+			// 
+			// groupBox2
+			// 
+			groupBox2.Anchor = AnchorStyles.Top;
+			groupBox2.Controls.Add(comboBox2);
+			groupBox2.Controls.Add(textBox8);
+			groupBox2.Controls.Add(textBox7);
+			groupBox2.Controls.Add(textBox6);
+			groupBox2.Controls.Add(textBox5);
+			groupBox2.Controls.Add(label10);
+			groupBox2.Controls.Add(label9);
+			groupBox2.Controls.Add(label8);
+			groupBox2.Controls.Add(label7);
+			groupBox2.Controls.Add(label6);
+			groupBox2.Controls.Add(button2);
+			groupBox2.Font = new Font("Segoe UI", 10F);
+			groupBox2.Location = new Point(508, 27);
+			groupBox2.Name = "groupBox2";
+			groupBox2.Size = new Size(408, 421);
+			groupBox2.TabIndex = 2;
+			groupBox2.TabStop = false;
+			groupBox2.Text = "Update Product";
+			groupBox2.Enter += groupBox2_Enter;
+			// 
+			// comboBox2
+			// 
+			comboBox2.FormattingEnabled = true;
+			comboBox2.Location = new Point(206, 281);
+			comboBox2.Name = "comboBox2";
+			comboBox2.Size = new Size(158, 31);
+			comboBox2.TabIndex = 7;
+			// 
+			// textBox8
+			// 
+			textBox8.Location = new Point(206, 227);
+			textBox8.Name = "textBox8";
+			textBox8.Size = new Size(158, 30);
+			textBox8.TabIndex = 7;
+			// 
+			// textBox7
+			// 
+			textBox7.Location = new Point(206, 176);
+			textBox7.Name = "textBox7";
+			textBox7.Size = new Size(158, 30);
+			textBox7.TabIndex = 7;
+			// 
+			// textBox6
+			// 
+			textBox6.Location = new Point(206, 123);
+			textBox6.Name = "textBox6";
+			textBox6.Size = new Size(158, 30);
+			textBox6.TabIndex = 7;
+			// 
+			// textBox5
+			// 
+			textBox5.Location = new Point(206, 76);
+			textBox5.Name = "textBox5";
+			textBox5.Size = new Size(158, 30);
+			textBox5.TabIndex = 7;
+			// 
+			// label10
+			// 
+			label10.AutoSize = true;
+			label10.Location = new Point(33, 286);
+			label10.Name = "label10";
+			label10.Size = new Size(79, 23);
+			label10.TabIndex = 7;
+			label10.Text = "Category";
+			// 
+			// label9
+			// 
+			label9.AutoSize = true;
+			label9.Location = new Point(33, 230);
+			label9.Name = "label9";
+			label9.Size = new Size(92, 23);
+			label9.TabIndex = 7;
+			label9.Text = "Supplier Id";
+			// 
+			// label8
+			// 
+			label8.AutoSize = true;
+			label8.Location = new Point(33, 179);
+			label8.Name = "label8";
+			label8.Size = new Size(140, 23);
+			label8.TabIndex = 7;
+			label8.Text = "Quantity in Stock";
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Location = new Point(33, 130);
+			label7.Name = "label7";
+			label7.Size = new Size(47, 23);
+			label7.TabIndex = 7;
+			label7.Text = "Price";
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Location = new Point(33, 79);
+			label6.Name = "label6";
+			label6.Size = new Size(121, 23);
+			label6.TabIndex = 7;
+			label6.Text = "Product Name";
+			label6.Click += label6_Click;
+			// 
+			// button2
+			// 
+			button2.AutoSize = true;
+			button2.BackColor = Color.DodgerBlue;
+			button2.FlatStyle = FlatStyle.Flat;
+			button2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold);
+			button2.ForeColor = Color.White;
+			button2.Location = new Point(244, 353);
+			button2.Name = "button2";
+			button2.Size = new Size(131, 51);
+			button2.TabIndex = 0;
+			button2.Text = "Update";
+			button2.UseVisualStyleBackColor = false;
+			// 
+			// groupBox3
+			// 
+			groupBox3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			groupBox3.Controls.Add(textBox9);
+			groupBox3.Controls.Add(label11);
+			groupBox3.Controls.Add(button3);
+			groupBox3.Font = new Font("Segoe UI", 10F);
+			groupBox3.Location = new Point(992, 27);
+			groupBox3.Name = "groupBox3";
+			groupBox3.Size = new Size(368, 242);
+			groupBox3.TabIndex = 2;
+			groupBox3.TabStop = false;
+			groupBox3.Text = "Delete Product";
+			groupBox3.Enter += groupBox3_Enter;
+			// 
+			// textBox9
+			// 
+			textBox9.Location = new Point(189, 76);
+			textBox9.Name = "textBox9";
+			textBox9.Size = new Size(158, 30);
+			textBox9.TabIndex = 8;
+			// 
+			// label11
+			// 
+			label11.AutoSize = true;
+			label11.Location = new Point(32, 79);
+			label11.Name = "label11";
+			label11.Size = new Size(90, 23);
+			label11.TabIndex = 8;
+			label11.Text = "Product Id";
+			// 
+			// button3
+			// 
+			button3.AutoSize = true;
+			button3.BackColor = Color.DodgerBlue;
+			button3.FlatStyle = FlatStyle.Flat;
+			button3.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold);
+			button3.ForeColor = Color.White;
+			button3.Location = new Point(216, 165);
+			button3.Name = "button3";
+			button3.Size = new Size(131, 51);
+			button3.TabIndex = 0;
+			button3.Text = "Delete";
+			button3.UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			button4.Anchor = AnchorStyles.Right;
+			button4.AutoSize = true;
+			button4.BackColor = Color.DodgerBlue;
+			button4.FlatStyle = FlatStyle.Flat;
+			button4.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold);
+			button4.ForeColor = Color.White;
+			button4.Location = new Point(974, 381);
+			button4.Name = "button4";
+			button4.Size = new Size(149, 51);
+			button4.TabIndex = 3;
+			button4.Text = "Export to Excel";
+			button4.UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			button5.Anchor = AnchorStyles.Right;
+			button5.AutoSize = true;
+			button5.BackColor = Color.DodgerBlue;
+			button5.FlatStyle = FlatStyle.Flat;
+			button5.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold);
+			button5.ForeColor = Color.White;
+			button5.Location = new Point(1208, 380);
+			button5.Name = "button5";
+			button5.Size = new Size(131, 51);
+			button5.TabIndex = 4;
+			button5.Text = "Back";
+			button5.UseVisualStyleBackColor = false;
+			button5.Click += button5_Click;
+			// 
+			// Form2
+			// 
+			AutoScaleDimensions = new SizeF(120F, 120F);
+			AutoScaleMode = AutoScaleMode.Dpi;
+			BackColor = Color.FromArgb(174, 217, 249);
+			ClientSize = new Size(1411, 883);
+			Controls.Add(button5);
+			Controls.Add(button4);
+			Controls.Add(groupBox3);
+			Controls.Add(groupBox2);
+			Controls.Add(groupBox1);
+			Controls.Add(dataGridView1);
+			FormBorderStyle = FormBorderStyle.Fixed3D;
+			Name = "Form2";
+			Padding = new Padding(5);
+			Text = "Form2";
+			WindowState = FormWindowState.Maximized;
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			groupBox1.ResumeLayout(false);
+			groupBox1.PerformLayout();
+			groupBox2.ResumeLayout(false);
+			groupBox2.PerformLayout();
+			groupBox3.ResumeLayout(false);
+			groupBox3.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private Button button1;
-        private Button button2;
-    }
+		private DataGridView dataGridView1;
+		private GroupBox groupBox1;
+		private GroupBox groupBox2;
+		private GroupBox groupBox3;
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private Label label1;
+		private TextBox textBox3;
+		private TextBox textBox2;
+		private TextBox textBox1;
+		private Label label5;
+		private Label label4;
+		private Label label3;
+		private Label label2;
+		private ComboBox comboBox1;
+		private TextBox textBox4;
+		private Label label6;
+		private TextBox textBox5;
+		private Label label10;
+		private Label label9;
+		private Label label8;
+		private Label label7;
+		private ComboBox comboBox2;
+		private TextBox textBox8;
+		private TextBox textBox7;
+		private TextBox textBox6;
+		private TextBox textBox9;
+		private Label label11;
+		private Button button4;
+		private Button button5;
+	}  
 }
